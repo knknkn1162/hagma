7ddee7698737a13141abf44428e0d9e8efcaeb32
knknkn <knknkn1162@gmail.com>
2018-04-04 23:00:00 +0900

    modify Hagma::ModuleInfo#initialize with using Backtrace::Location

--- lib/hagma/module_info.rb
+++ lib/hagma/module_info.rb
@@ -1,10 +1,13 @@
 module Hagma
   class ModuleInfo
     attr_reader :mod, :owner, :hook
-    def initialize(mod, owner, hook)
+    BACKTRACE_METHOD_NUMBER = 5
+    def initialize(mod, owner, hook, backtrace = true)
       @mod = mod
       @owner = owner
       @hook = hook
+      # trace before Hagma::Hook::module_event
+      @backtrace_locations = Backtrace::Location.locations(BACKTRACE_METHOD_NUMBER) if backtrace
     end
   end
 end
