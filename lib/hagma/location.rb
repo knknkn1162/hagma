e7edd9b5d981acb69e742426fe80935f4dd45bb4
knknkn <knknkn1162@gmail.com>
2018-04-05 22:57:36 +0900

    add attr_reader in Hagma::Location

--- lib/hagma/location.rb
+++ lib/hagma/location.rb
@@ -1,6 +1,7 @@
 module Hagma
   # generalized class in Thread::Backtrace::Location & return value of (Method|UnboundMethod)#source_location
   class Location
+    attr_reader :absolute_path, :base_label, :label, :lineno
     def initialize(**args)
       @absolute_path = args[:absolute_path]
       @base_label = args[:base_label]
