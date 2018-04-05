6c5c3720b49f2869d156e0d2c4f1db9de8082c74
knknkn <knknkn1162@gmail.com>
2018-04-05 20:53:49 +0900

    divide hagma.rb into two

--- lib/hagma.rb
+++ lib/hagma.rb
@@ -1,19 +1,3 @@
 require 'hagma/hook'
-
-# @author Kenta Nakajima
-# maybe use prepend than extend
-class Module
-  extend Hagma::Hook
-  method_event :method_added
-  method_event :method_removed
-  module_event :included
-  module_event :extended
-  module_event :prepended
-end
-
-# maybe use prepend than extend
-class BasicObject
-  extend ::Hagma::Hook
-  method_event :singleton_method_added
-  method_event :singleton_method_removed
-end
+# extend to Object & BasicObject class
+require 'hagma/ext'
---
+++ lib/hagma/ext.rb
@@ -0,0 +1,17 @@
+# @author Kenta Nakajima
+# maybe use prepend than extend
+class Module
+  extend Hagma::Hook
+  method_event :method_added
+  method_event :method_removed
+  module_event :included
+  module_event :extended
+  module_event :prepended
+end
+
+# maybe use prepend than extend
+class BasicObject
+  extend ::Hagma::Hook
+  method_event :singleton_method_added
+  method_event :singleton_method_removed
+end
