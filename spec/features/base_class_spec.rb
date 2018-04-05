db100a94ff09a4a11d0c1e0e2f275225ab08ea8a
knknkn <knknkn1162@gmail.com>
2018-04-05 23:03:06 +0900

    refactor method_added spec

--- spec/features/base_class_spec.rb
+++ spec/features/base_class_spec.rb
@@ -3,19 +3,20 @@ require 'test_case/base_class'
 
 RSpec.describe TestCase::BaseClass do
   # evaluate only once
-  let!(:methods) do
-    Hagma::Events.methods.map { |m| [m.klass, m.name, m.hook] }
+  let!(:events) { Hagma::Events.methods }
+  let(:method_suites) do
+    events.map { |m| [m.klass, m.name, m.hook] }
   end
 
-  context 'when BaseClass#base_instance_method' do
+  context 'when Object#method_added @ BaseClass#base_instance_method' do
     it 'belongs to Events.methods' do
-      expect(methods).to include [TestCase::BaseClass, :base_instance_method, :method_added]
+      expect(method_suites).to include [TestCase::BaseClass, :base_instance_method, :method_added]
     end
   end
 
-  context 'when BaseClass#base_singleton_method' do
+  context 'when Object#singleton_method_added @ BaseClass#base_singleton_method' do
     it 'belongs to Events.methods' do
-      expect(methods).to include [TestCase::BaseClass, :base_singleton_method, :singleton_method_added]
+      expect(method_suites).to include [TestCase::BaseClass, :base_singleton_method, :singleton_method_added]
     end
   end
 end
