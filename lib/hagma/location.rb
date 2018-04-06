25560872d41ab2be841b31d71a55b1b9baab8ed9
knknkn <knknkn1162@gmail.com>
2018-04-06 23:27:09 +0900

    fix Hagma::Location

--- lib/hagma/location.rb
+++ lib/hagma/location.rb
@@ -1,17 +1,22 @@
 require 'hagma/source_analyzer'
-require 'forwardable'
 
 module Hagma
   # generalized class in Thread::Backtrace::Location & return value of (Method|UnboundMethod)#source_location
   class Location
-    attr_reader :absolute_path, :base_label
-    extend Forwardable
-    delegate %i[absolute_path lineno] => :@source_analyzer
+    attr_reader :label, :base_label
 
     def initialize(**args)
       @base_label = args[:base_label]
       @label = args[:label]
       @source_analyzer = SourceAnalyzer.new(args[:absolute_path], args[:lineno]) if args[:absolute_path]
+    end
+
+    def absolute_path
+      @source_analyzer&.absolute_path
+    end
+
+    def lineno
+      @source_analyzer&.absolute_path
     end
   end
 end
