903c9c32feccfd9cfa71758c998dc94d866d4ee1
knknkn <knknkn1162@gmail.com>
2018-04-07 23:05:16 +0900

    add method_event in ext.rb

--- lib/hagma/ext.rb
+++ lib/hagma/ext.rb
@@ -4,6 +4,7 @@ class Module
   extend Hagma::Hook
   method_event :method_added
   method_event :method_removed
+  method_event :method_undefined
   module_event :included
   module_event :extended
   module_event :prepended
@@ -14,4 +15,5 @@ class BasicObject
   extend ::Hagma::Hook
   method_event :singleton_method_added
   method_event :singleton_method_removed
+  method_event :singleton_method_undefined
 end
