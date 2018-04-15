ff1cb65f60bb562affb1d0801264da5582444b7c
knknkn <knknkn1162@gmail.com>
2018-04-15 16:09:43 +0900

    fix base_inherited test_case

--- spec/features/base_inherited_spec.rb
+++ spec/features/base_inherited_spec.rb
@@ -3,61 +3,40 @@ require 'test_case/base_inherited'
 
 RSpec.describe TestCase::BaseInherited do
   # evaluate only once
-  let(:klass) { TestCase::BaseInherited }
+  let(:inherited_klass) { TestCase::BaseInherited }
+  let(:super_klass) { TestCase::SuperClass }
   let!(:method_evs) { Hagma::MethodInfo.method_collection }
   let(:method_suites) do
-    method_evs[klass].map { |m| [m.owner, m.name, m.hook] }
+    method_evs[inherited_klass].map { |m| [m.owner, m.name, m.hook] }
   end
 
-  context 'when array size of method_suites' do
-    it 'is 2' do
-      expect(method_suites.size).to eq 2
+  context 'when Hagma::Events::Summary#stat' do
+    let(:inherited_instance_m) { method_evs[inherited_klass].find { |m| m.name == :common_instance_method } }
+    let(:inherited_singleton_m) { method_evs[inherited_klass].find { |m| m.name == :common_singleton_method } }
+    let(:super_instance_m) { method_evs[super_klass].find { |m| m.name == :common_instance_method } }
+    let(:super_singleton_m) { method_evs[super_klass].find { |m| m.name == :common_singleton_method } }
+
+    let(:summary) { Hagma::Events::Summary.new Hagma::MethodInfo.method_collection, Hagma::ModuleInfo.module_collection }
+    let(:stats) { summary.klass_stats klass }
+
+    let(:instance_stats) do
+      [Hagma::Events::Summary::MethodStat.new(inherited_instance_m, inherited_klass, 0),
+       Hagma::Events::Summary::MethodStat.new(super_instance_m, super_klass, 1)]
+    end
+
+    let(:singleton_stats) do
+      [Hagma::Events::Summary::MethodStat.new(inherited_singleton_m, inherited_klass, 0),
+       Hagma::Events::Summary::MethodStat.new(super_singleton_m, super_klass, 1)]
+    end
+
+    it 'is true when instance_method' do
+      expect(stats[:instance].keys).to eq %i[common_instance_method singleton_method_added singleton_method_removed singleton_method_undefined]
+      expect(stats[:instance][:common_instance_method]).to eq instance_stats
+    end
+
+    it 'is true when singleton_method' do
+      expect(stats[:singleton].keys).to eq %i[common_singleton_method]
+      expect(stats[:singleton][:common_singleton_method]).to eq singleton_stats
     end
   end
-
-  # context 'when Object#method_added @ BaseClass#base_instance_method' do
-    # it 'belongs to Events.methods' do
-      # expect(method_suites).to include [klass, :base_instance_method, :method_added]
-    # end
-  # end
-
-  # context 'when Object#singleton_method_added @ BaseClass#base_singleton_method' do
-    # it 'belongs to Events.methods' do
-      # expect(method_suites).to include [klass, :base_singleton_method, :singleton_method_added]
-    # end
-  # end
-
-  # context 'when Hagma::MethodInfo#analyze method' do
-    # let(:method_info) do
-      # method_evs[klass].find do |m|
-        # [m.owner, m.name, m.hook] == [klass, :base_singleton_method, :singleton_method_added]
-      # end
-    # end
-    # it 'coincides source_location & head of caller_locations' do
-      # method_info.analyze
-      # source_location = method_info.instance_variable_get(:@location)
-      # caller_location = method_info.instance_variable_get(:@backtrace_locations)[0]
-      # expect([source_location.absolute_path, source_location.lineno]).to eq [caller_location.absolute_path, caller_location.lineno]
-    # end
-  # end
-
-  # context 'when Hagma::Events::Summary#stat' do
-    # let(:instance_m) { method_evs[klass].find { |m| m.name == :base_instance_method } }
-    # let(:singleton_m) { method_evs[klass].find { |m| m.name == :base_singleton_method } }
-    # let(:summary) { Hagma::Events::Summary.new Hagma::MethodInfo.method_collection, Hagma::ModuleInfo.module_collection }
-    # let(:stats) { summary.klass_stats TestCase::BaseClass }
-
-    # let(:instance_stat) { Hagma::Events::Summary::MethodStat.new(instance_m, klass, 0) }
-    # let(:singleton_stat) { Hagma::Events::Summary::MethodStat.new(singleton_m, klass, 0) }
-
-    # it 'is true when instance_method' do
-      # expect(stats[:instance].keys).to eq %i[base_instance_method singleton_method_added singleton_method_removed singleton_method_undefined]
-      # expect(stats[:instance][:base_instance_method]).to eq [instance_stat]
-    # end
-
-    # it 'is true when singleton_method' do
-      # expect(stats[:singleton].keys).to eq %i[base_singleton_method]
-      # expect(stats[:singleton][:base_singleton_method]).to eq [singleton_stat]
-    # end
-  # end
 end
--- spec/test_case/base_inherited.rb
+++ spec/test_case/base_inherited.rb
@@ -1,16 +1,16 @@
 module TestCase
   # :nodoc:
   class SuperClass
-    def base_instance_method; end
+    def common_instance_method; end
     class << self
-      def base_singleton_method; end
+      def common_singleton_method; end
     end
   end
 
   class BaseInherited < SuperClass
-    def inherited_instance_method; end
+    def common_instance_method; end
     class << self
-      def inherited_singleton_method; end
+      def common_singleton_method; end
     end
   end
 end
