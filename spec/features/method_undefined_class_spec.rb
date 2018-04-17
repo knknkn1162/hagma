d2017c7e5223e74860740bb8b4b9024b7a64d6fb
knknkn <knknkn1162@gmail.com>
2018-04-17 20:09:15 +0900

    modify specs

--- spec/features/method_undefined_class_spec.rb
+++ spec/features/method_undefined_class_spec.rb
@@ -1,28 +1,32 @@
 require 'hagma'
 require 'test_case/method_undefined_class'
 
-RSpec.describe TestCase::MethodUndefinedClass do
+RSpec.describe TestCase::MethodRemovedClass do
   let(:klass) { TestCase::MethodUndefinedClass }
-  let!(:events) { Hagma::MethodInfo.method_collection }
+  let(:singleton) { klass.singleton_class }
+  let!(:method_evs) { Hagma::MethodInfo.method_collection }
   let(:method_suites) do
-    events[klass].map { |m| [m.name, m.owner, m.hook] }
+    method_evs[kls].map { |m| [m.name, m.owner, m.hook] }
   end
 
   context 'when array size of method_suites' do
     it 'is 4' do
-      expect(method_suites.size).to eq 4
+      expect(method_evs[klass].size).to eq 2
+      expect(method_evs[singleton].size).to eq 2
     end
   end
 
-  context 'when Object#method_undefined @ MethodUndefinedClass#base_instance_method' do
+  context 'when MethodRemovedClass#base_instance_method' do
+    let(:kls) { klass }
     it 'belongs to Events.methods' do
       expect(method_suites).to include [:base_instance_method, klass, :method_undefined]
     end
   end
 
-  context 'when BasicObject#singleton_method_undefined @ MethodUndefinedClass#base_singleton_method' do
+  context 'when MethodRemovedClass#base_singleton_method' do
+    let(:kls) { singleton }
     it 'belongs to Events.methods' do
-      expect(method_suites).to include [:base_singleton_method, klass, :singleton_method_undefined]
+      expect(method_suites).to include [:base_singleton_method, singleton, :method_undefined]
     end
   end
 end
