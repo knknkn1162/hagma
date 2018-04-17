4863dd6ce25cfc8b26502e8b9ce09071f73d3bdb
knknkn <knknkn1162@gmail.com>
2018-04-17 19:53:50 +0900

    refactor base_module_spec

--- spec/features/base_module_spec.rb
+++ spec/features/base_module_spec.rb
@@ -3,34 +3,37 @@ require 'test_case/base_module'
 
 RSpec.describe TestCase::BaseModule do
   # evaluate only once
-  let(:mod) { TestCase::BaseModule }
+  let(:klass) { TestCase::BaseModule }
+  let(:singleton) { klass.singleton_class }
   let!(:method_evs) { Hagma::MethodInfo.method_collection }
-  let(:method_suites) do
-    method_evs[mod].map { |m| [m.owner, m.name, m.hook] }
-  end
+  let(:method_suites) { [method_info.owner, method_info.name, method_info.hook] }
 
   context 'when array size of method_suites' do
     it 'is 2' do
-      expect(method_suites.size).to eq 2
+      expect(method_evs.keys).to include klass, singleton
+      expect(method_evs[klass].size).to eq 1
+      expect(method_evs[singleton].size).to eq 1
     end
   end
 
-  context 'when Object#method_added @ BaseModule#base_instance_method' do
+  context 'when BaseClass#base_instance_method' do
+    let(:method_info) { method_evs[klass][0] }
     it 'belongs to Events.methods' do
-      expect(method_suites).to include [mod, :base_instance_method, :method_added]
+      expect(method_suites).to eq [klass, :base_instance_method, :method_added]
     end
   end
 
-  context 'when Object#singleton_method_added @ BaseModule#base_singleton_method' do
+  context 'when BaseClass::base_singleton_method' do
+    let(:method_info) { method_evs[singleton][0] }
     it 'belongs to Events.methods' do
-      expect(method_suites).to include [mod, :base_singleton_method, :singleton_method_added]
+      expect(method_suites).to eq [singleton, :base_singleton_method, :method_added]
     end
   end
 
   context 'when Hagma::MethodInfo#analyze method' do
     let(:method_info) do
-      method_evs[mod].find do |m|
-        [m.owner, m.name, m.hook] == [mod, :base_singleton_method, :singleton_method_added]
+      method_evs[singleton].find do |m|
+        [m.owner, m.name, m.hook] == [singleton, :base_singleton_method, :method_added]
       end
     end
     it 'coincides source_location & head of caller_locations' do
