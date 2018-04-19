3ae5ce3d44b71e6d8f6486a712d6557ed8cd5fba
knknkn <knknkn1162@gmail.com>
2018-04-19 20:06:35 +0900

    fix ModuleInfo#ancestors

--- lib/hagma/module_info.rb
+++ lib/hagma/module_info.rb
@@ -8,24 +8,29 @@ module Hagma
 
       def chain(owner)
         @chain ||= {}
-        @chain[owner] ||= module_collection[owner][:backward].reverse + [new(nil, owner, nil)] + module_collection[owner][:forward].reverse
+        @chain[owner] ||= module_collection[owner][:backward].reverse + [new(nil, owner, nil, false)] + module_collection[owner][:forward].reverse
+      end
+
+      def _linked_ancestors(target_info)
+        chain(target_info.target).map do |ancestor_module_info|
+          if ancestor_module_info.target.nil?
+            target_info
+          else
+            _linked_ancestors ancestor_module_info
+          end
+        end
       end
 
       def linked_ancestors(owner)
-        chain(owner).map do |ancestor_module_info|
-          if ancestor_module_info.target.nil?
-            ancestor_module_info
-          else
-            linked_ancestors ancestor_module_info.target
-          end
-        end
+        _linked_ancestors(new(owner, nil, nil, false))
       end
 
       # @return [List[ModuleInfo]] get ancestors which element is ModuleInfo. To get the normal ancestors, exec ModuleInfo.ancestors(owner).map(&:owner).
       def ancestors(owner)
         @ancestors ||= {}
-        res = smart_ancestors(owner).flatten
-        @ancestors[owner] ||= res + res.last.owner.ancestors[1..-1].map { |klass| new(nil, klass, nil) }
+        res = linked_ancestors(owner).flatten
+        p res.last
+        @ancestors[owner] ||= res + res.last.target.ancestors[1..-1].map { |klass| new(klass, nil, nil) }
       end
     end
     attr_reader :target, :owner, :hook
