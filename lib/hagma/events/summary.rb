0f96c19661d46ddbfe9f6c2b951f1096787dc6df
knknkn <knknkn1162@gmail.com>
2018-04-21 09:59:34 +0900

    mv ModuleInfo::ancestors to Summary#ancestors

--- lib/hagma/events/summary.rb
+++ lib/hagma/events/summary.rb
@@ -22,6 +22,32 @@ module Hagma
         end
       end
 
+      def chain(owner)
+        @chain ||= {}
+        @chain[owner] ||= @module_collection[owner][:backward].reverse + [ModuleInfo.dummy] + @module_collection[owner][:forward].reverse
+      end
+
+      def _linked_ancestors(target_info)
+        chain(target_info.target).map do |ancestor_module_info|
+          if ancestor_module_info.target.nil?
+            target_info
+          else
+            _linked_ancestors ancestor_module_info
+          end
+        end
+      end
+
+      def linked_ancestors(owner)
+        _linked_ancestors(ModuleInfo.root(owner))
+      end
+
+      # @return [List[ModuleInfo]] get ancestors which element is ModuleInfo. To get the normal ancestors, exec ModuleInfo.ancestors(owner).map(&:owner).
+      def ancestors(owner)
+        @ancestors ||= {}
+        res = linked_ancestors(owner).flatten
+        @ancestors[owner] ||= res + res.last.target.ancestors[1..-1].map { |klass| ModuleInfo.new(klass, nil, nil) }
+      end
+
       # @param method_collection [list[MethodInfo]]
       # @param module_collection [list[ModuleInfo]]
       def initialize(method_collection, module_collection)
--- lib/hagma/module_info.rb
+++ lib/hagma/module_info.rb
@@ -6,32 +6,6 @@ module Hagma
         @module_collection ||= Hash.new { |h, k| h[k] = { backward: [], forward: [] } }
       end
 
-      def chain(owner)
-        @chain ||= {}
-        @chain[owner] ||= module_collection[owner][:backward].reverse + [dummy] + module_collection[owner][:forward].reverse
-      end
-
-      def _linked_ancestors(target_info)
-        chain(target_info.target).map do |ancestor_module_info|
-          if ancestor_module_info.target.nil?
-            target_info
-          else
-            _linked_ancestors ancestor_module_info
-          end
-        end
-      end
-
-      def linked_ancestors(owner)
-        _linked_ancestors(root(owner))
-      end
-
-      # @return [List[ModuleInfo]] get ancestors which element is ModuleInfo. To get the normal ancestors, exec ModuleInfo.ancestors(owner).map(&:owner).
-      def ancestors(owner)
-        @ancestors ||= {}
-        res = linked_ancestors(owner).flatten
-        @ancestors[owner] ||= res + res.last.target.ancestors[1..-1].map { |klass| new(klass, nil, nil) }
-      end
-
       def root(owner)
         new(owner, nil, nil, false)
       end
