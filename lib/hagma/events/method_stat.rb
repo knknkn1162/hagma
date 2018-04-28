6b3d760913dcc3ca3f07686283133f40297851f2
knknkn <knknkn1162@gmail.com>
2018-04-28 15:18:56 +0900

    rename instance_variables and add **_backtraces

--- lib/hagma/events/method_stat.rb
+++ lib/hagma/events/method_stat.rb
@@ -5,10 +5,18 @@ module Hagma
     # @param level Integer
     class MethodStat
       attr_reader :method_info, :owner, :level
-      def initialize(method_info, owner, hook)
+      def initialize(method_info, module_info, level)
         @method_info = method_info
-        @owner = owner
-        @hook = hook
+        @owner = module_info
+        @level = level
+      end
+
+      def method_backtraces
+        method_info.backtrace_locations
+      end
+
+      def module_backtraces
+        module_info.backtrace_locations
       end
     end
   end
