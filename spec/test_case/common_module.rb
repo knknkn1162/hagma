79658faff8a84e9fae158cfe309a4b35b99c3634
knknkn <knknkn1162@gmail.com>
2018-04-28 23:51:14 +0900

    delete unused test_case file

--- spec/test_case/common_module.rb
+++
@@ -1,8 +0,0 @@
-module TestCase
-  module CommonModule
-    def common_instance_method; end
-    class << self
-      def common_singleton_method; end
-    end
-  end
-end
--- spec/test_case/common_super_class.rb
+++
@@ -1,9 +0,0 @@
-module TestCase
-  # :nodoc:
-  class CommonSuperClass
-    def common_instance_method; end
-    class << self
-      def common_singleton_method; end
-    end
-  end
-end
