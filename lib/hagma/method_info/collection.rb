cf718ffc432669954a2b744016e00accb264e36f
knknkn <knknkn1162@gmail.com>
2018-04-29 22:54:33 +0900

    modify collection instance_variable in Hagma::MethodInfo::Collection

--- lib/hagma/method_info/collection.rb
+++ lib/hagma/method_info/collection.rb
@@ -6,24 +6,31 @@ module Hagma
     class Collection
       attr_reader :collection
       def initialize
-        @collection = Hash.new { |h, k| h[k] = [] }
+        @collection = Hash.new { |h1, k1| h1[k1] = Hash.new { |h2, k2| h2[k2] = [] } }
       end
 
       def push(method, owner, hook, access_controller = nil, backtrace = false)
-        @collection[owner] << MethodInfo.new(method, owner, hook, access_controller, backtrace)
+        @collection[owner][method] << MethodInfo.new(method, owner, hook, access_controller, backtrace)
       end
 
       def list
-        @list ||= @collection.map { |_, mets| mets }.flatten
+        @list = []
+        @list = @collection.map do |_, mets|
+          mets.map do |_met, method_info|
+            method_info
+          end
+        end.flatten
       end
 
       def owner_methods(owner)
-        @collection[owner]
+        @collection[owner].values
       end
 
       def merge!(other_collection)
-        other_collection.collection.each do |owner, methods|
-          @collection[owner] += methods
+        other_collection.collection.each do |owner, mets|
+          mets.each do |met, methods|
+            @collection[owner][met] += methods
+          end
         end
         self
       end
