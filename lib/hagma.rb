cc30738d114949387c5013a90b54323b4aa05504
knknkn <knknkn1162@gmail.com>
2018-04-29 14:46:46 +0900

    create core_ext/hook.rb to bundle extention to Object, Module, BasicObject

--- lib/hagma.rb
+++ lib/hagma.rb
@@ -1,8 +1,4 @@
 require 'hagma/hook'
 # store existed method, e.g) Object#taint..
 require 'hagma/initializer'
-
-# extend to Object & BasicObject class
-require 'hagma/core_ext/module/hook'
-require 'hagma/core_ext/class/hook'
-require 'hagma/core_ext/basic_object/hook'
+require 'hagma/core_ext/hook'
---
+++ lib/hagma/core_ext/hook.rb
@@ -0,0 +1,3 @@
+require 'hagma/core_ext/module/hook'
+require 'hagma/core_ext/class/hook'
+require 'hagma/core_ext/basic_object/hook'
