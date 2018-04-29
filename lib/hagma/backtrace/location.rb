3ae1928431221f8daf1188640bdcab102d6e5f5e
knknkn <knknkn1162@gmail.com>
2018-04-29 17:16:06 +0900

    refactor Backtrace::Location class

--- lib/hagma/backtrace/location.rb
+++ lib/hagma/backtrace/location.rb
@@ -4,15 +4,17 @@ module Hagma
   module Backtrace
     # parser class for Thread::Backtrace::Location
     class Location
-      def self.split(location)
-        location.class.instance_methods(false).map do |sym|
-          [sym, location.__send__(sym)]
-        end.to_h
-      end
+      class << self
+        def split(location)
+          location.class.instance_methods(false).map do |sym|
+            [sym, location.__send__(sym)]
+          end.to_h
+        end
 
-      def self.locations(start)
-        caller_locations(start).map do |loc|
-          Hagma::Location.new(**split(loc))
+        def locations(start)
+          caller_locations(start).map do |loc|
+            Hagma::Location.new(**split(loc))
+          end
         end
       end
     end
