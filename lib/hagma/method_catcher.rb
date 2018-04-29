8975a370f458ad36fa53464ff67bcdb628ae3bc0
knknkn <knknkn1162@gmail.com>
2018-04-29 13:34:15 +0900

    add merge! method in MethodCatcher

--- lib/hagma/method_catcher.rb
+++ lib/hagma/method_catcher.rb
@@ -5,11 +5,18 @@ module Hagma
   class MethodCatcher
     attr_reader :collection
     def initialize
-      @collection = []
+      @collection = Hash.new { |h, k| h[k] = [] }
     end
 
-    def push(method, owner, hook)
-      @collection << MethodInfo.new(method, owner, hook)
+    def push(method, owner, hook, access_controller = nil, backtrace = false)
+      @collection[owner] << MethodInfo.new(method, owner, hook, access_controller: access_controller, backtrace: backtrace)
+    end
+
+    def merge!(method_catcher)
+      method_catcher.collection.each do |owner, lst|
+        @collection[owner] += lst
+      end
+      self
     end
   end
 end
