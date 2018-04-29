8e3c1e5e1c5b22c15d605054c93efa166b2d739e
knknkn <knknkn1162@gmail.com>
2018-04-29 17:24:45 +0900

    refactor Hagma::ConstInfo

--- lib/hagma/const_info.rb
+++ lib/hagma/const_info.rb
@@ -1,4 +1,4 @@
-require 'hagma/method_catcher'
+require 'hagma/method_info/collection'
 
 module Hagma
   class ConstInfo
@@ -11,13 +11,15 @@ module Hagma
     end
 
     def collect_methods
+      collection = MethodInfo::Collection.new
       [const, const.singleton_class].each do |cst|
         %i[public protected private].each do |controller|
           cst.__send__("#{controller}_instance_methods", false).map do |met|
-            MethodCatcher.push(met, cst, :core, access_controller: controller, backtrace: false)
+            collection.push(met, cst, :core, controller)
           end
         end
       end
+      collection
     end
   end
 end
